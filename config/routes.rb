e224f1813bfd40e887b7378ed99aa47a82a78fee
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-04-05 18:27:49 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   #resources :students, only: :index :show
   get 'students', to: 'students#index'
-  get 'students', to: 'students#show'
+  get 'students/:id', to: 'students#show'
 end
